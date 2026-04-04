0306447eb89d7e6bb062d1e0a55b1c24300c9315
sirviumesh100 <umesirvi500@gmail.com>
2026-04-04 10:45:29 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,58 @@
-from sensors import start_mqtt
+
+# ------ Code for Web Dashboard Interface -------
+
+from flask import Flask, render_template, request, jsonify
+from data_manager import get_combined_data
+from ai_advisor import generate_ai_advice
+import socket
+
+app = Flask(__name__)
+
+
+# ===============================
+# HOME PAGE
+# ===============================
+
+@app.route("/")
+def home():
+    data = get_combined_data()
+    return render_template("index.html", data=data)
+
+
+# ===============================
+# AI CHAT ENDPOINT
+# ===============================
+
+@app.route("/ask", methods=["POST"])
+def ask():
+    farmer_question = request.json.get("question")
+
+    combined_data = get_combined_data()
+    answer = generate_ai_advice(combined_data, farmer_question)
+
+    return jsonify({"answer": answer})
+
+# ===============================
+# RUN SERVER
+# ===============================
+
+if __name__ == "__main__":
+    hostname = socket.gethostname()
+    local_ip = socket.gethostbyname(hostname)
+
+    print("\n===================================")
+    print(f"🌐 Open this in browser:")
+    print(f"http://{local_ip}:5000")
+    print("===================================\n")
+
+    app.run(host="0.0.0.0", port=5000, debug=False)
+
+
+
+
+
+
+'''from sensors import start_mqtt
 from data_manager import get_combined_data
 from ai_advisor import generate_ai_advice
 import time
@@ -13,3 +67,4 @@ answer = generate_ai_advice(data, farmer_question)
 time.sleep(1)
 print("\nAI Response:\n")
 print(answer)
+'''
